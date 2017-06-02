a09f01f13fdd1a32eef493339303bbcd78f91893
athmanziri <issaziri@gmail.com>
2017-06-02 15:59:06 +0300

    changed Africastalking api to python3

--- venv2/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ziri/Desktop/Swahilipot_projects/sms_platform/sms/venv2/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
