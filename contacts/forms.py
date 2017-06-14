2cef9ab3b39e0b81c68e6f208e8f68377750fba3
bmwasaru <bmwasaru@gmail.com>
2017-06-14 13:31:18 +0300

    More PEP8

--- contacts/forms.py
+++ contacts/forms.py
@@ -4,29 +4,34 @@ from .models import Contact, Contact_Group
 
 
 class Contact_GroupForm(forms.ModelForm):
-	name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
 
-	class Meta:
-		model = Contact_Group
-		fields = ('name',)
+    class Meta:
+        model = Contact_Group
+        fields = ('name', )
 
 
 class ContactForm(forms.ModelForm):
-	first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	mobile = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	category = forms.ModelChoiceField(queryset=None, 
-									widget=forms.Select(attrs={'class':'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    mobile = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    category = forms.ModelChoiceField(
+        queryset=None, widget=forms.Select(attrs={'class': 'form-control'}))
 
-	class Meta:
-		model  = Contact
-		fields = ('first_name', 'last_name', 'email', 'mobile', 'category')
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'email', 'mobile', 'category')
 
-		
-	def __init__(self, user, *args, **kwargs):
-	    super(ContactForm, self).__init__(*args, **kwargs)
-	    self.fields['category'].queryset = Contact_Group.objects.filter(user=user)
+    def __init__(self, user, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['category'].queryset = Contact_Group.objects.filter(
+            user=user)
 
 
 class UploadFileForm(forms.Form):
